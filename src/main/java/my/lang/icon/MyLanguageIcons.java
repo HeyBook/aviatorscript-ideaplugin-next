40cd5ab7e0514a34c11093092e7e2f8a9647ca4a
zhengmingliang <1072307340@qq.com>
2022-06-26 00:09:03 +0800

    适配idea2022.1.2

--- src/main/java/my/lang/icon/MyLanguageIcons.java
+++ src/main/java/my/lang/icon/MyLanguageIcons.java
@@ -1,6 +1,7 @@
 package my.lang.icon;
 
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.ReflectionUtil;
 
 import javax.swing.*;
 
@@ -8,9 +9,9 @@ import javax.swing.*;
  * @author yanchangyou
  */
 public class MyLanguageIcons {
+    static Class<?> callerClass = ReflectionUtil.getGrandCallerClass();
+    public static final Icon FILE = IconLoader.findIcon("my/lang/icon/av.svg",callerClass.getClassLoader());
 
-    public static final Icon FILE = IconLoader.findIcon("my/lang/icon/av.svg");
-
-    public static final Icon STATEMENT = IconLoader.findIcon("my/lang/icon/action.svg");
+    public static final Icon STATEMENT = IconLoader.findIcon("my/lang/icon/action.svg",callerClass.getClassLoader());
 
 }
